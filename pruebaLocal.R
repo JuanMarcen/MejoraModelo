5c32294355d9cd53cee671d9e3c89a9f7e3e2279
JuanMarcen <jumarguti2002@gmail.com>
2025-06-30 12:33:02 +0200

    Commit from Rstudio

--- pruebaLocal.R
+++ pruebaLocal.R
@@ -52,34 +52,10 @@ rho_q0.95 <- sum(mod_q0.95$residuals<0) / length(ind)
 
 # Día 
 library(lubridate)
+source('functions.R')
 
-day_month <- unique(format(Y$Date, "%d-%m"))
-df_dia <- matrix(NA, nrow=length(day_month), ncol=2)
-df_dia <- as.data.frame(df_dia, row.names = day_month)
-colnames(df_dia) <- c('rho_l_q0.5', 'rho_l_q0.95')
-
-for (i in 1:length(day_month)){
-  
-  day <- which(format(Y$Date[ind], "%d-%m") == day_month[i])
-  df_dia[i,'rho_l_q0.5']<-sum(mod_q0.5$residuals[day] < 0, na.rm=T) / 64
-  df_dia[i,'rho_l_q0.95']<-sum(mod_q0.95$residuals[day] < 0, na.rm=T) / 64
-  
-}
-
-# Años 
-year <- unique(year(Y$Date[ind]))
-df_year <- matrix(NA, nrow=length(year), ncol=2)
-df_year <- as.data.frame(df_year, row.names = year)
-colnames(df_year) <- c('rho_t_q0.5','rho_t_q0.95')
-
-for (i in 1:length(year)){
-  
-  j <- which(year(Y$Date[ind]) == year[i])
-  df_year[i,'rho_t_q0.5'] <- sum(mod_q0.5$residuals[j] < 0, na.rm=T) / 92
-  df_year[i,'rho_t_q0.95'] <- sum(mod_q0.95$residuals[j] < 0, na.rm=T) / 92
-  
-}
-
+df_dia <- rho_day(mod_q0.5, mod_q0.95)
+df_year <- rho_year(mod_q0.5, mod_q0.95)
 
 # Gráficos
 setwd('C:/Users/jumar/OneDrive/Escritorio/Github/MejoraModelo')
