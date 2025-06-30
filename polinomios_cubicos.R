f95aed5190e98260c1cf2f1d31c33438653695ec
JuanMarcen <jumarguti2002@gmail.com>
2025-06-30 12:33:02 +0200

    Commit from Rstudio

--- polinomios_cubicos.R
+++ polinomios_cubicos.R
@@ -1,6 +1,3 @@
-# Improving existing models
-# example in one station
-
 rm(list=ls())
 
 # Data uploading
@@ -13,44 +10,32 @@ Y <- readRDS("C:/Users/jumar/OneDrive/Escritorio/TFM/Datos/Y.rds")
 # Subset Madrid (Retiro)
 ind <- which(Y$station==230)
 
-# Pruebas
-vars_q0.5 <- ifelse(grepl("^I\\(.*\\)$", vars_q0.5),
-                    paste0("`", vars_q0.5, "`"),
-                    vars_q0.5)
-vars_q0.95 <- ifelse(grepl("^I\\(.*\\)$", vars_q0.95),
-                     paste0("`", vars_q0.95, "`"),
-                     vars_q0.95)
-
-formula_q0.5 <- as.formula(
-  paste('Y~', paste(
-    vars_q0.5,collapse = '+'
-  ),'+ I(sin(2*pi*l/365)) + I(cos(2*pi*l/365))')
-)
-formula_q0.95 <- as.formula(
-  paste('Y~', paste(
-    vars_q0.95,collapse = '+'
-  ),'+ I(sin(2*pi*l/365)) + I(cos(2*pi*l/365))')
-)
-
-
-# Modelos
+# Adding 3rd degree polynomials
 library(quantreg)
-mod_q0.5 <- rq(formula_q0.5,data=df_conj_filled_sc,subset=ind,tau=0.50)
-mod_q0.95 <- rq(formula_q0.95,data=df_conj_filled_sc,subset=ind,tau=0.95)
+
+formula <- as.formula(
+  paste('Y ~', paste(colnames(df_conj_filled_sc)[2:16], collapse = '+'), '+',
+  paste0('`', colnames(df_conj_filled_sc)[17:46], '`', collapse = '+'))
+  )
+
+mod_nulo_q0.5 <- rq(Y ~ 1, data = df_conj_filled_sc, subset = ind, tau = 0.5)
+mod_q0.5<-step(mod_nulo_q0.5, scope = formula, direction = 'forward',
+               pen = log(length(ind)))
+
+mod_nulo_q0.95 <- rq(Y ~ 1, data = df_conj_filled_sc, subset = ind, tau = 0.95)
+mod_q0.95<-step(mod_nulo_q0.95, scope = formula, direction = 'forward',
+               pen = log(length(ind)))
+
 
 # R1
-mod_nulo_q0.5 <- rq(Y~1,data=df_conj_filled_sc,subset=ind,tau=0.5)  
-mod_nulo_q0.95 <- rq(Y~1,data=df_conj_filled_sc,subset=ind,tau=0.95) 
-
 R1_q0.5 <- 1 - mod_q0.5$rho / mod_nulo_q0.5$rho
 R1_q0.95 <- 1 - mod_q0.95$rho / mod_nulo_q0.95$rho
 
-# rho
 
-rho_q0.5 <- sum(mod_q0.5$residuals<0) / length(ind)
-rho_q0.95 <- sum(mod_q0.95$residuals<0) / length(ind)
+# Rho
+rho_q0.5 <- sum(mod_q0.5$residuals < 0) / length(ind)
+rho_q0.95 <- sum(mod_q0.95$residuals < 0) / length(ind)
 
-# Día 
 library(lubridate)
 source('functions.R')
 
@@ -59,20 +44,19 @@ df_year <- rho_year(mod_q0.5, mod_q0.95)
 
 # Gráficos
 setwd('C:/Users/jumar/OneDrive/Escritorio/Github/MejoraModelo')
-png("solo_armonicos.png", width = 1400, height = 600, res = 150)
+png("pol_gr_3", width = 1400, height = 600, res = 150)
 par(mfrow = c(1,2))
 plot(1:92, df_dia$rho_l_q0.5, type='l', 
-     main = 'Madrid (Retiro) (días) (armónicos)',
+     main = 'Madrid (Retiro) (días) (pol gr3)',
      ylab = expression(rho[l](tau)), xlab = 'l', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
 lines(1:92, df_dia$rho_l_q0.95)
 abline(h = 0.95, col = 'red')
 
 plot(1:64, df_year$rho_t_q0.5, type='l', 
-     main = 'Madrid (Retiro) (años) (armónicos)',
+     main = 'Madrid (Retiro) (años) (pol gr3)',
      ylab = expression(rho[t](tau)), xlab = 't', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
 lines(1:64, df_year$rho_t_q0.95)
 abline(h = 0.95, col = 'red')
 dev.off()
-
