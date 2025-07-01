2592e6efbd736f7514af26b3c7dad0590cd61451
JuanMarcen <jumarguti2002@gmail.com>
2025-07-01 10:09:59 +0200

    Commit from Rstudio

--- functions.R
+++ functions.R
@@ -1,36 +1,40 @@
 # Functions
 
 # Rho by days and years
-rho_day <- function(model_q1, model_q2){
+rho_day <- function(model_q1, model_q2, df){
   
-  day_month <- unique(format(Y$Date, "%d-%m"))
+  day_month <- unique(df$l)
   df_dia <- matrix(NA, nrow=length(day_month), ncol=2)
   df_dia <- as.data.frame(df_dia, row.names = day_month)
   colnames(df_dia) <- c('rho_l_q0.5', 'rho_l_q0.95')
   
+  year <- unique(df$t)
+  
   for (i in 1:length(day_month)){
     
-    day <- which(format(Y$Date[ind], "%d-%m") == day_month[i])
-    df_dia[i,'rho_l_q0.5']<-sum(model_q1$residuals[day] < 0, na.rm=T) / 64
-    df_dia[i,'rho_l_q0.95']<-sum(model_q2$residuals[day] < 0, na.rm=T) / 64
+    day <- which(df$l == day_month[i])
+    df_dia[i,'rho_l_q0.5']<-sum(model_q1$residuals[day] < 0, na.rm=T) / length(year)
+    df_dia[i,'rho_l_q0.95']<-sum(model_q2$residuals[day] < 0, na.rm=T) / length(year)
     
   }
  
   return(df_dia) 
 }
 
-rho_year <- function(model_q1, model_q2){
+rho_year <- function(model_q1, model_q2, df){
   
-  year <- unique(year(Y$Date[ind]))
+  year <- unique(df$t)
   df_year <- matrix(NA, nrow=length(year), ncol=2)
   df_year <- as.data.frame(df_year, row.names = year)
   colnames(df_year) <- c('rho_t_q0.5','rho_t_q0.95')
   
+  day_month <- unique(df$l)
+  
   for (i in 1:length(year)){
     
-    j <- which(year(Y$Date[ind]) == year[i])
-    df_year[i,'rho_t_q0.5'] <- sum(model_q1$residuals[j] < 0, na.rm=T) / 92
-    df_year[i,'rho_t_q0.95'] <- sum(model_q2$residuals[j] < 0, na.rm=T) / 92
+    j <- which(df$t == year[i])
+    df_year[i,'rho_t_q0.5'] <- sum(model_q1$residuals[j] < 0, na.rm=T) / length(day_month)
+    df_year[i,'rho_t_q0.95'] <- sum(model_q2$residuals[j] < 0, na.rm=T) / length(day_month)
     
   }
   
--- lags.R
+++ lags.R
@@ -5,14 +5,15 @@ Y <- readRDS("C:/Users/jumar/OneDrive/Escritorio/TFM/Datos/Y.rds")
 
 ind <- which(Y$station==230)
 
+library(lubridate)
 df_madrid <- df_conj_filled_sc[ind,]
+df_madrid$t <- year(Y$Date[ind]) - 1960 + 1
 
 
 # Dataframe with lags
 library(dplyr)
 
 df_madrid <- df_madrid %>%
-  mutate(t = rep(1960:2023, each = 92)) %>%
   group_by(t) %>%
   mutate(across(all_of(names(df_madrid)[2:46]), ~lag(.), .names = "{.col}_lag")) %>%
   ungroup() %>%
@@ -31,7 +32,7 @@ formula <- as.formula(paste(
 library(quantreg)
 mod_nulo_q0.5 <- rq(Y ~ 1, data = df_madrid, tau = 0.5)
 mod_q0.5<-step(mod_nulo_q0.5, scope = formula, direction = 'forward',
-               pen = log(length(ind)-64))
+               pen = log(dim(df_madrid)[1]))
 
 mod_nulo_q0.95 <- rq(Y ~ 1, data = df_madrid, tau = 0.95)
 mod_q0.95<-step(mod_nulo_q0.95, scope = formula, direction = 'forward',
@@ -44,30 +45,29 @@ R1_q0.95 <- 1 - mod_q0.95$rho / mod_nulo_q0.95$rho
 
 
 # Rho
-rho_q0.5 <- sum(mod_q0.5$residuals < 0) / (length(ind) - 64)
-rho_q0.95 <- sum(mod_q0.95$residuals < 0) / (length(ind) - 64)
+rho_q0.5 <- sum(mod_q0.5$residuals < 0) / (dim(df_madrid)[1])
+rho_q0.95 <- sum(mod_q0.95$residuals < 0) / (dim(df_madrid)[1])
 
-library(lubridate)
 source('functions.R')
 
-df_dia <- rho_day(mod_q0.5, mod_q0.95)
-df_year <- rho_year(mod_q0.5, mod_q0.95)
+df_dia <- rho_day(mod_q0.5, mod_q0.95, df_madrid)
+df_year <- rho_year(mod_q0.5, mod_q0.95, df_madrid)
 
 # Gráficos
 setwd('C:/Users/jumar/OneDrive/Escritorio/Github/MejoraModelo')
 png("lags.png", width = 1400, height = 600, res = 150)
 par(mfrow = c(1,2))
-plot(1:92, df_dia$rho_l_q0.5, type='l', 
+plot(unique(df_madrid$l), df_dia$rho_l_q0.5, type='l', 
      main = 'Madrid (Retiro) (días) (pol gr3)',
      ylab = expression(rho[l](tau)), xlab = 'l', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
-lines(1:92, df_dia$rho_l_q0.95)
+lines(unique(df_madrid$l), df_dia$rho_l_q0.95)
 abline(h = 0.95, col = 'red')
 
-plot(1:64, df_year$rho_t_q0.5, type='l', 
+plot(unique(df_madrid$t), df_year$rho_t_q0.5, type='l', 
      main = 'Madrid (Retiro) (años) (pol gr3)',
      ylab = expression(rho[t](tau)), xlab = 't', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
-lines(1:64, df_year$rho_t_q0.95)
+lines(unique(df_madrid$t), df_year$rho_t_q0.95)
 abline(h = 0.95, col = 'red')
 dev.off()
