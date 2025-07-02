4c6b68ba108b36e0537feba03850bbf65896ac13
JuanMarcen <jumarguti2002@gmail.com>
2025-07-02 17:23:23 +0200

    commit

--- lags.R
+++ lags.R
@@ -41,6 +41,10 @@ mod_nulo_q0.5 <- rq(Y ~ 1, data = df_madrid, tau = 0.5)
 mod_q0.5<-step(mod_nulo_q0.5, scope = formula, direction = 'forward',
                pen = log(dim(df_madrid)[1]))
 
+mod_nulo_q0.5_l <- lm(Y ~ 1, data = df_madrid)
+mod_q0.5_l<-step(mod_nulo_q0.5_l, scope = formula, direction = 'forward',
+               pen = log(dim(df_madrid)[1]))
+
 mod_nulo_q0.95 <- rq(Y ~ 1, data = df_madrid, tau = 0.95)
 mod_q0.95<-step(mod_nulo_q0.95, scope = formula, direction = 'forward',
                 pen = log(length(ind)))
@@ -59,6 +63,11 @@ source('functions.R')
 
 df_dia <- rho_day(mod_q0.5, mod_q0.95, df_madrid)
 df_year <- rho_year(mod_q0.5, mod_q0.95, df_madrid)
+df_geop <- rho_geop(mod_q0.5, mod_q0.95, df_madrid, 'g300', 50)
+
+df_dia_p <- rho_day(mod_q0.5, mod_q0.95, df_madrid, extra = T)
+df_year_m <- rho_year(mod_q0.5, mod_q0.95, df_madrid, extra = T)
+
 
 # Gráficos
 setwd('C:/Users/jumar/OneDrive/Escritorio/Github/MejoraModelo')
@@ -68,6 +77,13 @@ plot(unique(df_madrid$l), df_dia$rho_l_q0.5, type='l',
      main = 'Madrid (Retiro) (días) (pol gr3)',
      ylab = expression(rho[l](tau)), xlab = 'l', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.5_p1, col = 'blue')
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.5_p2, col = 'forestgreen')
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.5_p3, col = 'purple')
+lines(unique(df_madrid$l), df_dia$rho_l_q0.95)
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.95_p1, col = 'blue')
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.95_p2, col = 'forestgreen')
+lines(unique(df_madrid$l), df_dia_p$rho_l_q0.95_p3, col = 'purple')
 lines(unique(df_madrid$l), df_dia$rho_l_q0.95)
 abline(h = 0.95, col = 'red')
 
@@ -75,6 +91,13 @@ plot(unique(df_madrid$t), df_year$rho_t_q0.5, type='l',
      main = 'Madrid (Retiro) (años) (pol gr3)',
      ylab = expression(rho[t](tau)), xlab = 't', ylim = c(0, 1))
 abline(h = 0.5, col = 'red')
+lines(1:64, df_year_m$rho_t_q0.5_jun, col = 'blue')
+lines(1:64, df_year_m$rho_t_q0.5_jul, col = 'forestgreen')
+lines(1:64, df_year_m$rho_t_q0.5_ag, col = 'purple')
+lines(1:64, df_year$rho_t_q0.95)
+lines(1:64, df_year_m$rho_t_q0.95_jun, col = 'blue')
+lines(1:64, df_year_m$rho_t_q0.95_jul, col = 'forestgreen')
+lines(1:64, df_year_m$rho_t_q0.95_ag, col = 'purple')
 lines(unique(df_madrid$t), df_year$rho_t_q0.95)
 abline(h = 0.95, col = 'red')
 dev.off()
